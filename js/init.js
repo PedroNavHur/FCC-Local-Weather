bf31807e326047335f31ab3f0e390beacdbc91ec
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-07-06 14:08:39 -0600

    DarkSky Api Test

--- js/init.js
+++ js/init.js
@@ -2,12 +2,9 @@
   $(function() {
 
     // we define the global variables
-    var position = {
-      latitude: null,
-      longitud: null
-    };
+    var position;
 
-    // we call the get Current Time function
+    // we call the get functions
     getLocationInfo();
     getCurrentTime();
 
@@ -28,6 +25,7 @@
 
         // set the info in the html
         $("#location").html(country + ', ' + city);
+        getCurrentWeather();
 
       }, "jsonp");
 
@@ -73,6 +71,130 @@
       $("#time").html(dayS + ', ' + hour + ":" + minute);
 
     }
+
+    // Current Weather function
+    function getCurrentWeather() {
+
+      // we define the local variables
+      var apiKey = "0a00ea58cefe0998cff62dd8d9506ce9/";
+      var url = "https://api.darksky.net/forecast/";
+
+      // weather local variables
+      var weather;
+      var icon;
+      var temperature;
+      var humidity;
+      var windSpeed;
+      var rainProv;
+
+      // weather variables calc
+      var temperatureHtml;
+      var iconHtml;
+      var humidityHtml;
+      var rainProvHtml;
+      var windSpeedHtml;
+
+      // test the forecast on console
+      console.log(
+        url + apiKey + position
+      );
+
+      // JSONP darksky info request
+      $.get(url + apiKey + position, function(response) {
+
+        // asigns the info to the variables
+        weather = response.currently.summary;
+        icon = response.currently.icon;
+        temperature = response.currently.temperature;
+        humidity = response.currently.humidity;
+        windSpeed = response.currently.windSpeed;
+        rainProv = response.currently.precipProbability;
+
+        // test the wether info on console
+        console.log(
+          weather + icon + temperature + humidity + windSpeed + rainProv
+        );
+
+        // switch to choose the icon
+        switch (icon) {
+          case 'clear-day':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'clear-night':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'rain':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'snow':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'sleet':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'wind':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'fog':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'cloudy':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'partly-cloudy-day':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          case 'partly-cloudy-night':
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+            break;
+          default:
+            iconHtml = '<i class="wi wi-day-rain"></i>';
+        }
+
+        // if to change from imperial to metric
+        if ($('#conversionSwitch').prop('checked')) {
+          console.log('metric');
+          temperature = ((temperature - 32) * (5/9));
+          temperature = parseInt(temperature);
+          temperatureHtml = (temperature + '<i class="wi wi-celsius"></i> ');
+          windSpeed = (windSpeed / 0.62137);
+          windSpeed = parseInt(windSpeed);
+          windSpeedHtml = ('<i class="wi wi-strong-wind"></i> ' + windSpeed + 'Kph')
+        } else {
+          console.log('imperial');
+          temperature = parseInt(temperature);
+          temperatureHtml = (temperature + '<i class="wi wi-fahrenheit"></i> ')
+          windSpeed = parseInt(windSpeed);
+          windSpeedHtml = ('<i class="wi wi-strong-wind"></i> ' + windSpeed + 'Mph')
+        }
+
+        // convert humidity and rainProv
+        humidity = (humidity * 100);
+        humidity = parseInt(humidity);
+        humidityHtml = ('<i class="wi wi-humidity"></i> ' + humidity + ' %')
+        rainProv = (rainProv * 100);
+        rainProv = parseInt(rainProv);
+        rainProvHtml = ('<i class="wi wi-umbrella"></i> ' + rainProv + ' %')
+
+        // we show the info in html
+        $("#weather").html(weather);
+        $("#temperature").html(temperatureHtml + iconHtml);
+        $("#humidity").html(humidityHtml);
+        $("#rain").html(rainProvHtml);
+        $("#wind").html(windSpeedHtml);
+
+      }, "jsonp");
+
+    }
+
+    $('#conversionSwitch').on('click', function() {
+      if ($('#conversionSwitch').prop('checked')) {
+        console.log('metric');
+      } else {
+        console.log('imperial');
+      }
+      getCurrentWeather();
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
